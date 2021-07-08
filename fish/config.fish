2e7c756c65482127dfce6fe08825cfaae1fae975
Marius Kleiner <kleiner@gmail.com>
2021-07-08 10:38:12 +0200

    Migrate default editor env var to fish

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,9 @@ set fish_greeting ""
 # Always use UTF-8 for everything
 set -x LC_ALL "en_US.UTF-8"
 
+# My beloved default editor
+set -x EDITOR /usr/local/bin/vim
+
 # Include user's personal bin directory in the path
 test -d $HOME/bin && fish_add_path -p -P $HOME/bin
 
