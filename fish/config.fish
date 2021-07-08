4c7a99621c386453874f269824e82fd291aefabd
Marius Kleiner <kleiner@gmail.com>
2021-07-08 10:32:07 +0200

    Home bin path and locale config for fish
    Note the need for the '-P' option to fish_add_path. Without this, fish
    would consider this a change to an universal variable and make changes
    to fish/fish_variables. However, I want this command to keep working
    even if the value of the HOME environment variable should ever change,
    so I need -P to directly change the path instead.

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,11 @@
 # Disable standard greeting message
 set fish_greeting ""
 
+# Always use UTF-8 for everything
+set -x LC_ALL "en_US.UTF-8"
+
+# Include user's personal bin directory in the path
+test -d $HOME/bin && fish_add_path -p -P $HOME/bin
+
 # Make less behave properly with ansi codes
 alias less='less -R'
